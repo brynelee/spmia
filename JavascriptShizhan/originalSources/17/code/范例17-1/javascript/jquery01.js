d043f8ad20489bb6082f829a1bcc9805a2030be0
Xiaodong Li <bryne_lxd@sina.com>
2019-03-17 23:49:34 +0800

    remove JavascriptShizhan original sources from the repo.

--- JavascriptShizhan/originalSources/17/code/范例17-1/javascript/jquery01.js
+++
@@ -1,6 +0,0 @@
-	$(function(){
-		$('input').click(function(){    //获取input类型组件，然后设置该组件的单击事件
-			alert("第一个jQuery程序！");           //弹出对话框
-		});
-	});
-                                                                                                        
--- JavascriptShizhan/originalSources/22/22-6.config.js
+++
@@ -1,2 +0,0 @@
-exports.root = "webroot/";			//主目录
-exports.index = "index.html";			//默认主页
